37414fb1a9ef8ed41857b354c344a218cdad39ee
ZeroLevelKing <2200011028@stu.pku.edu.cn>
2025-06-19 22:16:46 +0800

    doc part 3

--- src/config.py
+++ src/config.py
@@ -6,16 +6,16 @@ class Config:
         self.nx = 200
         self.x_min = -5.0
         self.x_max = 5.0
-        self.t_end = 1.0
+        self.t_end = 2.0
         
         # 物理参数
         self.gamma = 1.4
         self.cv = 1.0
         
         # 数值方法选择
-        self.scheme = 'tvd'       # 激波捕捉格式: 'tvd', 'gvc', 'weno'
+        self.scheme = 'weno'       # 激波捕捉格式: 'tvd', 'gvc', 'weno'
         self.flux_method = 'fvs'   # 通量处理方法: 'fvs', 'fds'
-        self.flux_type = 'van_leer'  
+        self.flux_type = 'van_leer' 
                                    # 具体通量类型: 
                                    # FVS: 'steger_warming', 'van_leer', 'ausm', 'lax_friedrichs'
                                    # FDS: 'hll', 'lax_wendroff', 'roe（存在bug）'
@@ -30,7 +30,7 @@ class Config:
         self.cfl = 0.5
         
         # 输出控制
-        self.output_interval = 0.1  # 输出间隔时间
+        self.output_interval = 0.5  # 输出间隔时间
         self.save_plots = True       # 是否保存图像
         self.plot_dir = "results"    # 图像保存目录
     
